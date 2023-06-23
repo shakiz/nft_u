5b084f946d44e804078c3931b2a928de16c9a221
shakiz <activities335@gmail.com>
2023-06-23 16:50:18 +0600

    Add remote and local data source impl

---
+++ app/src/main/java/com/digital/nftu/data/sourceimpl/LocalSourceImpl.kt
@@ -0,0 +1,6 @@
+package com.digital.nftu.data.sourceimpl
+
+import com.digital.nftu.data.source.LocalSource
+
+class LocalSourceImpl : LocalSource {
+}
---
+++ app/src/main/java/com/digital/nftu/data/sourceimpl/RemoteSourceImpl.kt
@@ -0,0 +1,6 @@
+package com.digital.nftu.data.sourceimpl
+
+import com.digital.nftu.data.source.RemoteSource
+
+class RemoteSourceImpl : RemoteSource {
+}
