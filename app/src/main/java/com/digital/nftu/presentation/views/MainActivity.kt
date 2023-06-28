6acb33f9e71179ae4fac97a80e9104669b73d196
shakiz <activities335@gmail.com>
2023-06-28 11:23:05 +0600

    Add toolbar menu and item click in home fragment

--- app/src/main/java/com/digital/nftu/presentation/views/MainActivity.kt
+++ app/src/main/java/com/digital/nftu/presentation/views/MainActivity.kt
@@ -1,18 +1,14 @@
 package com.digital.nftu.presentation.views
 
 import android.os.Bundle
-import android.view.Menu
-import android.view.MenuInflater
-import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.MenuProvider
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
 import com.digital.nftu.R
 import com.digital.nftu.databinding.ActivityMainBinding
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
-class MainActivity : AppCompatActivity(), MenuProvider {
+class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
@@ -26,13 +22,5 @@ class MainActivity : AppCompatActivity(), MenuProvider {
         val navController = findNavController(R.id.nav_host_fragment_activity_main)
         binding.navView.itemIconTintList = null
         navView.setupWithNavController(navController)
-    }
-
-    override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
-        menuInflater.inflate(R.menu.home_menu, menu)
-    }
-
-    override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
-        return false
     }
 }
--- app/src/main/java/com/digital/nftu/presentation/views/home/HomeFragment.kt
+++ app/src/main/java/com/digital/nftu/presentation/views/home/HomeFragment.kt
@@ -1,21 +1,26 @@
 package com.digital.nftu.presentation.views.home
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import android.widget.TextView
+import android.widget.Toast
+import androidx.core.view.MenuHost
+import androidx.core.view.MenuProvider
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.ViewModelProvider
+import com.digital.nftu.R
 import com.digital.nftu.databinding.FragmentHomeBinding
+import com.digital.nftu.presentation.views.MainActivity
 
 class HomeFragment : Fragment() {
 
     private var _binding: FragmentHomeBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -33,6 +38,41 @@ class HomeFragment : Fragment() {
             textView.text = it
         }
         return root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setupToolbar()
+        prepareToolbarMenus()
+    }
+
+    private fun setupToolbar(){
+        (requireActivity() as MainActivity).setSupportActionBar(binding.toolbar)
+    }
+
+    private fun prepareToolbarMenus() {
+        (requireActivity() as MenuHost).addMenuProvider(object : MenuProvider {
+            override fun onPrepareMenu(menu: Menu) {
+                super.onPrepareMenu(menu)
+            }
+
+            override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
+                menuInflater.inflate(R.menu.home_menu, menu)
+            }
+
+            override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
+                return when(menuItem.itemId){
+                    R.id.navigation_notifications -> {
+                        true
+                    }
+                    R.id.navigation_wallet -> {
+                        true
+                    }
+                    else -> false
+                }
+            }
+        }, viewLifecycleOwner, Lifecycle.State.RESUMED)
     }
 
     override fun onDestroyView() {
