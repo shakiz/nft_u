a80771c5dd5127e1d3d2a714939e52474552cf31
shakiz <activities335@gmail.com>
2023-07-18 00:10:44 +0600

    Fix container view margin

--- app/src/main/java/com/digital/nftu/presentation/views/MainActivity.kt
+++ app/src/main/java/com/digital/nftu/presentation/views/MainActivity.kt
@@ -2,7 +2,9 @@ package com.digital.nftu.presentation.views
 
 import android.os.Bundle
 import android.view.View
+import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.FragmentContainerView
 import com.digital.nftu.R
 import com.digital.nftu.databinding.ActivityMainBinding
 import com.google.android.material.bottomappbar.BottomAppBar
@@ -28,10 +30,16 @@ class MainActivity : AppCompatActivity() {
             findViewById<BottomAppBar>(R.id.bottomAppBar).visibility = View.VISIBLE
             findViewById<BottomNavigationView>(R.id.bottomNavigationView).visibility = View.VISIBLE
             findViewById<FloatingActionButton>(R.id.fab).visibility = View.VISIBLE
+            val param = findViewById<FragmentContainerView>(R.id.navHostFragment).layoutParams as ViewGroup.MarginLayoutParams
+            param.bottomMargin = 152
+            findViewById<FragmentContainerView>(R.id.navHostFragment).layoutParams = param
         } else {
             findViewById<BottomAppBar>(R.id.bottomAppBar).visibility = View.GONE
             findViewById<BottomNavigationView>(R.id.bottomNavigationView).visibility = View.GONE
             findViewById<FloatingActionButton>(R.id.fab).visibility = View.GONE
+            val param = findViewById<FragmentContainerView>(R.id.navHostFragment).layoutParams as ViewGroup.MarginLayoutParams
+            param.bottomMargin = 0
+            findViewById<FragmentContainerView>(R.id.navHostFragment).layoutParams = param
         }
     }
 }
